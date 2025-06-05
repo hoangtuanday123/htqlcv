b89b1f63cbdec6141df2323617cf4042257d6c09
tuan.phan <pnhtuanhcmus@gmail.com>
2025-06-05 09:09:45 -0700

    build maven and build docker

--- backend/src/main/java/com/example/htqlCV/Configuration/redisConfig.java
+++ backend/src/main/java/com/example/htqlCV/Configuration/redisConfig.java
@@ -1,18 +1,23 @@
 package com.example.htqlCV.Configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-
 @Configuration
 @EnableRedisRepositories
 public class redisConfig {
-    private String redisHost = "localhost";
-    private int redisPort = 6379;
-     @Bean
+    // private String redisHost = "localhost";
+    // private int redisPort = 6379;
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+    @Bean
     public JedisConnectionFactory jedisConnectionFactory() {
         RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
         config.setHostName(redisHost);
